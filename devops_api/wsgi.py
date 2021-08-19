e24e269bcb4f0e9165978efd4d9447bfa92ebda6
往事随风 <gujiwork@outlook.com>
2021-08-19 16:17:41 +0800

    Update wsgi.py

--- devops_api/wsgi.py
+++ devops_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pigs-devops-apps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devops_api.settings')
 
 application = get_wsgi_application()
